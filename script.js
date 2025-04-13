af5026ff2dd4d415b62d3af96aa17b305c612c5a
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2025-04-13 12:45:55 +0530

    localstorage

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 let checkBoxes = document.querySelectorAll(".circle");
 let inputBoxes = document.querySelectorAll("input");
 let error = document.querySelector(".error");
-let progress = document.querySelector('.progress');
-let count = 0;
-let taskCompleted = 0;
+let progress = document.querySelector(".progress");
+let count = 3;
+let taskCompleted;
+
+let allGoals = JSON.parse(localStorage.getItem("allGoals")) || {};
+
 checkBoxes.forEach((element) => {
   element.addEventListener("click", () => {
     let img = element.querySelector(".check-icon");
@@ -24,13 +27,31 @@ checkBoxes.forEach((element) => {
       inputClass.classList.toggle("inputs");
       error.style.display = "none";
       img.classList.toggle("check-completed");
-      count += 33.33;
-      progress.style.width = `${count}%`;
-      taskCompleted++;
-      progress.textContent = `${taskCompleted}/3 completed`;
-
+      let inputId = element.nextElementSibling.id;
+      allGoals[inputId].completed = !allGoals[inputId].completed;
+      localStorage.setItem("allGoals", JSON.stringify(allGoals));
+      if (allGoals[inputId].completed) {
+        taskCompleted = Object.values(allGoals).filter(
+          (count) => count.completed
+        ).length;
+        progress.textContent = `${taskCompleted}/3 completed`;
+        taskCompleted = (taskCompleted / count) * 100;
+        progress.style.width = `${taskCompleted}%`;
+      }
     } else {
       error.style.display = "block";
     }
+  });
+});
+inputBoxes.forEach((input) => {
+  if (allGoals[input.id]) {
+    input.value = allGoals[input.id].name;
+  }
+  input.addEventListener("input", (e) => {
+    allGoals[input.id] = {
+      name: input.value,
+      completed: false,
+    };
+    localStorage.setItem("allGoals", JSON.stringify(allGoals));
   });
 });
